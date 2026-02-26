6493b6ac22a134e4c08a3240ba140ee958afe63b
Rishikesh Dash <rishikeshdash03@gmail.com>
2026-02-26 16:00:11 +0530

    run implemented

--- src/main/java/Jfuck.java
+++ src/main/java/Jfuck.java
@@ -2,26 +2,69 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public static class Jfuck {
+public class Jfuck {
 
-    private static void run(String ip) {
-        System.out.println(ip);
+    private void run(String ip) {
+        //todo handle program_counter and size comparison
+        final int size = 30_000;
+        Program program = new Program(size);
+       for(char programCounter:  ip.toCharArray()) {
+           switch (programCounter) {
+               case '+':
+                   program.container[program.dataCounter]++;
+                   break;
+               case '-':
+                   program.container[program.dataCounter]--;
+                   break;
+               case '.':
+                    System.out.println((char) program.container[program.dataCounter]);
+                    break;
+               case ',':
+//                   todo handle input later
+                   Scanner sc =new Scanner(System.in);
+                   char in =  sc.next().charAt(0);
+                   program.container[program.dataCounter] = (int)in;
+                   break;
+               case '>':
+                   if(program.dataCounter <  size) {
+                       program.dataCounter++;
+                   }
+                   break;
+               case '<':
+                   if(program.dataCounter > 0) {
+                       program.dataCounter--;
+                   }
+                   break;
+               case '[':
+                   break;
+               case ']':
+                   break;
+               default:
+                   /*
+                    treated as comments
+                    */
+                   break;
+
+           }
+
+       }
     }
 
-    private static void repl() {
+    private void repl() {
         //todo replace string with something better
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
             while (true) {
                 System.out.print("bf> ");
                 String ip;
                 if ((ip = br.readLine()) != null) {
-                    if(ip.isEmpty())
+                    if (ip.isEmpty())
                         continue;
-                    if(ip.equals("exit")){
+                    if (ip.equals("exit")) {
                         System.out.println("exit");
                         break;
                     }
-                    run(ip);
+                    Jfuck jfuck = new Jfuck();
+                    jfuck.run(ip);
                 }
             }
         } catch (IOException e) {
@@ -30,11 +73,12 @@ public static class Jfuck {
     }
 }
 
-static void main(String[] args) {
+void main(String[] args) {
     System.out.println("Hello World!");
 
     if (args.length == 0) {
-        Jfuck.repl();
+        Jfuck jfuck = new Jfuck();
+        jfuck.repl();
     } else if (args.length == 1) {
         try (BufferedReader br = new BufferedReader(new FileReader(args[0]))) {
             String line;
@@ -42,9 +86,20 @@ static void main(String[] args) {
             while ((line = br.readLine()) != null) {
                 sb.append(line);
             }
-            Jfuck.run(sb.toString());
+            Jfuck jfuck = new Jfuck();
+            jfuck.run(sb.toString());
         } catch (IOException e) {
             System.err.println("Error reading file: " + e.getMessage());
         }
+    }
+}
+
+class Program{
+    int[] container;
+    int dataCounter;
+
+    Program(int size) {
+        container = new int[size];
+        this.dataCounter = 0;
     }
 }
